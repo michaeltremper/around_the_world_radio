c57e2b2238f83a1471d4516444c8882d758376c0
michaeltremper <hmichaeltremper@gmail.com>
2023-10-31 01:41:22 -0700

    Added AntennaAngle version 1

--- AntennaAngle.py
+++ AntennaAngle.py
@@ -3,25 +3,30 @@ from rtlsdr import RtlSdr
 import matplotlib.pyplot as plt
 
 # Configuration
-center_freq = 100e6  # Center frequency in Hz
-sample_rate = 2.048e6  # Sample rate in Hz
-num_samples = 1024  # Number of samples per capture
+center_freq = 7.074e6  # Center frequency in Hz
+sample_rate = 2.4e6  # Sample rate in Hz
+num_samples = 1024 * 1024  # Number of samples per capture
 num_rotations = 12  # Number of antenna rotations
 angles = np.linspace(0, 360, num_rotations)
+direct_sampling_mode = 2 if center_freq < 14e6 else 0
 
 # Initialize RTL-SDR
 sdr = RtlSdr()
+sdr.set_direct_sampling(direct_sampling_mode)
 sdr.sample_rate = sample_rate
 sdr.center_freq = center_freq
 sdr.gain = 'auto'
 
 # Initialize arrays to store results
 signal_strengths = []
+msg_str = f"Rotate antenna {360/num_rotations} degrees. <press enter>"
 
 for angle in angles:
     # Rotate antenna, capture samples, calculate signal strength
-    
-    signal_strengths.append()
+    samples = sdr.read_samples(num_samples)
+    signal_strength_dB = 10 * np.log10(np.mean(np.abs(samples)**2))
+    signal_strengths.append(signal_strength_dB)
+    input(msg_str)
 
 # Close the RTL-SDR device
 sdr.close()
